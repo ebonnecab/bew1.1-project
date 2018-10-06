d4f98a9f9d7769c220afb861e89c31ed537be473
Ebonne Cabarrus <cabarrus.ebonne@gmail.com>
2018-10-06 11:35:17 -0700

    character index

--- app.js
+++ app.js
@@ -7,13 +7,23 @@ var exphbs = require('express-handlebars');
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
 
-//Root route
+
+// Array of Rick and Morty Characters
+let character = [
+    { name: "Rick Sanchez", episode: "Episode 5" },
+    { name: "Pickle Rick", episode: "Episode 2"}
+]
+
+// ROOT ROUTE
 app.get('/', (req, res) => {
-    res.render('home', { msg: 'Handlebars are so cool!'});
+    res.render('character-index', { character: character});
 })
 
 app.listen(3000, () => {
     console.log('App listening on port 3000!')
 })
 
+
+
 module.exports = app;
+
