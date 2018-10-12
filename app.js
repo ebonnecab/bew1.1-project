fa59344ac52b5cd724a38f61d1d385727f749b0b
Ebonne Cabarrus <cabarrus.ebonne@gmail.com>
2018-10-11 18:33:37 -0700

    restful api

--- app.js
+++ app.js
@@ -25,9 +25,8 @@ app.set('view engine', 'handlebars');
 
 app.use('/', character)
 
-app.listen(process.env.PORT || 3000, () => {
-    console.log('App listening on port 3000!')
-})
+const port = process.env.PORT || 3000;
+app.listen(port);
 
 
 
